8dc926ea109e74ee6dc1c4175c085b7b9569ca92
brapastor <brapastor@gmail.com>
2020-04-17 03:19:44 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//1 import { init } from "./js/chistes-page";
+// import { obtenerUsuarios } from "./js/http-provider";
+// init();
+
+//2 import { init } from "./js/usuarios-page";
+// init();
+
+// import * as CRUD from './js/crud-provider'
+import { init } from './js/archivos-page'
+
+// CRUD.getUsuario(2).then(console.log)
+
+// CRUD.crearUsuario({
+//     name: 'Brayan',
+//     job: 'programador'
+// }).then(console.log)
+
+// CRUD.actualizarUsuario(1,{
+//     name: 'Pool',
+//     job: 'ing civil'
+// }).then(console.log)
+
+// CRUD.borrarUsuario(1).then(console.log)
+
+init();
